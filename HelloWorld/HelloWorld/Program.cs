7324d3aa21399dbcef0b2e9e882fe43326828b08
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-08-15 20:31:01 -0300

    feat: agregar app de consola (hello world)

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args) // Main es el entry point. Al ejecutar el programa, se ejecuta Main.
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
